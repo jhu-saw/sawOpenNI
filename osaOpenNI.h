5c79d5a0cb7fbe57b3ad247484f376b0b1273766
Kel Guerin <kguerin2@jhu.edu>
2012-02-06 19:08:44 +0000

    osaOpenNI.h -- updated some #defines regarding skeleton joint labels
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk/saw/components/sawOpenNI@3423 0959eb1a-6956-4f60-947b-6092a4e22c97

--- osaOpenNI.h
+++ osaOpenNI.h
@@ -43,6 +43,35 @@ http://www.cisst.org/cisst/license.txt.
 #define CNI_USR_WAIT        7
 #define CNI_USR_IDLE        -1
 
+#define CNI_HEA     1
+#define CNI_NEK     2     //XN_SKEL_NECK			= 2,
+#define CNI_TOR     3     //XN_SKEL_TORSO			= 3,
+#define CNI_WAI     4     //XN_SKEL_WAIST			= 4,
+
+#define CNI_LCO     5     //XN_SKEL_LEFT_COLLAR		= 5,
+#define CNI_LSH     6     //XN_SKEL_LEFT_SHOULDER	= 6,
+#define CNI_LEL     7     //XN_SKEL_LEFT_ELBOW		= 7,
+#define CNI_LWR     8     //XN_SKEL_LEFT_WRIST		= 8,
+#define CNI_LHN     9     //XN_SKEL_LEFT_HAND		= 9,
+#define CNI_LFI     10    //XN_SKEL_LEFT_FINGERTIP	=10,
+
+#define CNI_RCO     11    //XN_SKEL_RIGHT_COLLAR	=11,
+#define CNI_RSH     12    //XN_SKEL_RIGHT_SHOULDER	=12,
+#define CNI_REL     13    //XN_SKEL_RIGHT_ELBOW		=13,
+#define CNI_RWR     14    //XN_SKEL_RIGHT_WRIST		=14,
+#define CNI_RHN     15    //XN_SKEL_RIGHT_HAND		=15,
+#define CNI_RFI     16    //XN_SKEL_RIGHT_FINGERTIP	=16,
+
+#define CNI_LHP     17    //XN_SKEL_LEFT_HIP		=17,
+#define CNI_LKN     18    //XN_SKEL_LEFT_KNEE		=18,
+#define CNI_LAN     19    //XN_SKEL_LEFT_ANKLE		=19,
+#define CNI_LFT     20    //XN_SKEL_LEFT_FOOT		=20,
+
+#define CNI_RHP     21    //XN_SKEL_RIGHT_HIP		=21,
+#define CNI_RKN     22    //XN_SKEL_RIGHT_KNEE		=22,
+#define CNI_RAN     23    //XN_SKEL_RIGHT_ANKLE		=23,
+#define CNI_RFT     24     //XN_SKEL_RIGHT_FOOT		=24	
+
 
 /*!
   \todo move ctor code to Configure method
